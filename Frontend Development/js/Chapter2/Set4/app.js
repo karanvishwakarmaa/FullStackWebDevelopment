b6303d76bc1dd20d019842641657e7340a9454ed
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-17 20:49:43 +0530

    Day 17 if-else statements

---
+++ Frontend Development/js/Chapter2/Set4/app.js
@@ -0,0 +1,11 @@
+console.log("Before my if statement")
+
+let age = 19;
+
+if (age >= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are not an adult");
+}  // This line is not part of the if statement. It's outside the curly braces.
+
+console.log("After my if statement")
