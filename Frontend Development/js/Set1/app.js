b734ec2ba4e4a959eb8ee589f49c6d4bf0aad9a6
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-17 15:20:34 +0530

    Day 17 Starting JS

---
+++ Frontend Development/js/Set1/app.js
@@ -0,0 +1,14 @@
+// console.log("Hello World!!!")
+
+// let a =10;
+// let b = 5;
+
+// console.log("the sun is: ",a + b)
+
+let pencilPrice = 10
+let erasorPrice = 5
+
+// let totalCost = pencilPrice + erasorPrice
+let output = `Your Total Amount: ${pencilPrice+erasorPrice} Ruppees...}`
+
+console.log(output)
