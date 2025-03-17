415e3764c8323c68f34a2cf04fb48a4824162f1d
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-17 15:29:20 +0530

    Day 17

---
+++ Frontend Development/js/Set2/app.js
@@ -0,0 +1,29 @@
+//arithmetic operator
+
+// const num1 = 10;
+// const num2 = 5;
+
+// const sum = num1 + num2;
+// const difference = num1 - num2;
+
+let a = 10;
+let b = 222;
+
+let sum = a + b;
+let difference = a - b;
+let multi = a * b;
+let divide = a / b;
+let modules = a % b;
+let power = a ** b;
+
+console.log(`The sum of ${a} and ${b} is ${sum}`);
+
+console.log(`The difference of ${a} and ${b} is ${difference}`);
+
+console.log(`The product of ${a} and ${b} is ${multi}`);
+
+console.log(`The quotient of ${a} and ${b} is ${divide}`);
+
+console.log(`The remainder of ${a} and ${b} is ${modules}`);
+
+console.log(`The power of ${a} to ${b} is ${power}`);
