fcaa17f86abc192fe3fd4306f421db53c35c42e7
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-18 14:57:26 +0530

    Day 18 ( else-if statement)

--- Frontend Development/js/Chapter2/Set5/app.js
+++ Frontend Development/js/Chapter2/Set5/app.js
@@ -1,9 +1,13 @@
 console.log("Before my if statement")
-age = 18
-if (age >= 18) {
-    console.log("Your can vote now...!!!")
-} else {
-    console.log("You can't vote know...!!")
-}
 
+//traffic lights
+let color = "red";
+
+if (color === "red") {
+    console.log("Stop");
+} else if (color === "green") {
+    console.log("Go");
+} else{
+    console.log("Yield");
+}
 console.log("After my if statement")
