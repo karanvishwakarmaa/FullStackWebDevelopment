2ef09e3c6a58b748c6b1ea0abfeba62f1d8a2c92
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-18 14:48:58 +0530

    Day 18 (if-else statement)

---
+++ Frontend Development/js/Chapter2/Set5/app.js
@@ -0,0 +1,9 @@
+console.log("Before my if statement")
+age = 18
+if (age >= 18) {
+    console.log("Your can vote now...!!!")
+} else {
+    console.log("You can't vote know...!!")
+}
+
+console.log("After my if statement")
