79ed09d2b97951a1fc0b4f92f0f05d1c2b128050
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-19 22:49:56 +0530

    practice question

---
+++ Frontend Development/js/Chapter2/Practice-Question/set4/app.js
@@ -0,0 +1,11 @@
+let firstnumber = prompt("Enter first number")
+let secondnumber = prompt("Enter second number")
+let thirdnumber = prompt("Enter third number")
+
+if((firstnumber > secondnumber) && (firstnumber > thirdnumber)){
+    console.log("The largest number is: ", firstnumber);
+} else if((secondnumber > firstnumber) && (secondnumber > thirdnumber) {
+    console.log("The largest number is: ", secondnumber);
+} else {
+    console.log("The largest number is: ", thirdnumber);
+}
