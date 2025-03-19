941711b863bb38e446cf94698aff39a04f048ac7
karanvishwakarmaa <karanvishwakarma634@gmail.com>
2025-03-19 21:34:10 +0530

    Day 19 ( if-else statement)

---
+++ Frontend Development/js/Chapter2/Set6/app.js
@@ -0,0 +1,16 @@
+// let age = 19;
+// if(age >= 18) {
+//     console.log("You can vote")
+// } else if(age <= 18){
+//     console.log("You cannot Vote!!!")
+// }
+
+let marks = 75;
+
+if(marks >= 90) {
+    console.log("Grade: A")
+}
+
+else if(marks >= 80 && marks < 90) {
+    console.log("Grade: B")
+}
---
+++ Frontend Development/js/Chapter2/set7/app.js
@@ -0,0 +1,10 @@
+let popcorn = "XL";
+if(popcorn == "S"){
+    console.log("Enjoy your popcorn! and the price is Rs. 50");
+} else if(popcorn == "M") {
+    console.log("Enjoy your popcorn! and the price is Rs. 100")
+} else if(popcorn == "L") {
+    console.log("Enjoy your popcorn! and the price is Rs. 200")
+} else {
+    console.log("Enjoy your popcorn! and the price is Rs. 250")
+}
